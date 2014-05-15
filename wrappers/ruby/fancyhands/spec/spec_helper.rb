18c01c4cf513afeae6e8cd96e412702f3300cf30
Jose Casanova and Joe Sak <dev+jcasanova+joemsak@onlifehealth.com>
2014-05-14 23:40:06 -0400

    Filter key and secret out of cassettes

--- wrappers/ruby/fancyhands/spec/spec_helper.rb
+++ wrappers/ruby/fancyhands/spec/spec_helper.rb
@@ -5,6 +5,8 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/cassettes'
   c.hook_into :webmock
   c.configure_rspec_metadata!
+  c.filter_sensitive_data('<API_KEY>')    { ENV['FANCYHANDS_KEY'] }
+  c.filter_sensitive_data('<API_SECRET>') { ENV['FANCYHANDS_SECRET'] }
 end
 
 RSpec.configure do |config|
